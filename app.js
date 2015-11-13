4b7aa9f1e0d5f31da65e5c531849fb645e7a385e
RoseTHERESA <rose.minta.theresa@gmail.com>
2015-11-13 11:53:28 -0800

    submit incomplete

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var app = angular.module('todoApp', []); // What's missing? Hint, it's more than one thing.
+var app = angular.module('todoApp', []);
 
-app.controller("TodosController", function($scope) { // What's missing?
+app.controller("TodosController", function($scope) {
 	 // Store todo items here. What is the data type?
    $scope.todos = [];
 
-   $scope.addTodo = function() { // Rewrite to make this function abailable in your view
+   $scope.addTodo = function() {
 		// Add code here
     $scope.todos.push[""];
   };
   
 
-	$scope.deleteAll = function() { // Rewrite to make this function abailable in your view
+	$scope.deleteAll = function() { 
 		// Add code here
     $scope.todos = [];
 	};
